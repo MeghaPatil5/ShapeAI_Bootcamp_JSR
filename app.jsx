2b0e41c22a0c19bee62e17eccd4cdf6104190352
MeghaPatil5 <81710281+MeghaPatil5@users.noreply.github.com>
2021-06-06 11:50:34 +0530

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import App from "./Footer";
+import App from "./Note";
+
+
+function App(){
+	return (
+		<div>
+			<Header />
+			<Footer />
+			<Note />
+			<Note />
+			<Note />
+	    </div>
+	    );
+}
+
+export default App;
---
+++ note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note(){
+	return(
+	<div classname="note">
+	<h1>javascript and react.js</h1>
+	<p>This was an amazing bootcamp taken up by sir Shaurya sinha
+	We covered everythig from scratch including javascript and react.js
+	</p>
+	</div>
+	);
+}
+
+export default Note;
